eedf4e9255fbc8ec375f1ac54978bbacef97f085
Polina Koval <zagadka1316@gmail.com>
2021-10-06 12:05:28 +0300

    Add tests method __str__ in model User

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 
 
 class SignUpPageTests(TestCase):
@@ -25,3 +27,18 @@ class SignUpPageTests(TestCase):
             'password2': self.password
         })
         self.assertEqual(response.status_code, 302)
+
+
+class TestStr(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username="test_user", email="user1@test.com", is_staff=True)
+        self.user.set_password("password1")
+        self.user.save()
+
+    def test_str_is_equal_to_username(self):
+        self.client.force_authenticate(self.user)
+        test_user = User.objects.get(username='test_user')
+        print(str(test_user))
+        assert str(test_user) == test_user.username
