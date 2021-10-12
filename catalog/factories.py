38d845c8def1e473d198676d7140ec9ae36c51b1
Polina Koval <polina.koval@anvileight.com>
2021-10-12 11:13:42 +0300

    Rewrite base.html and other templates

--- catalog/factories.py
+++ catalog/factories.py
@@ -19,10 +19,7 @@ class BoardGamesFactory(factory.django.DjangoModelFactory):
     @factory.post_generation
     def category(self, create, extracted, **kwargs):
         if not create:
-            # Simple build, do nothing.
             return
 
         if extracted:
-            # A list of groups were passed in, use them
-            for category in extracted:
-                self.category.add(category)
+            self.category.add(*extracted)
