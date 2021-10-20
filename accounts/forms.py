aadf5eba5e71179c37ebdaf953637111b49ecf63
Polina Koval <polina.koval@anvileight.com>
2021-10-20 17:25:08 +0300

    Edit signup.html Only an authorized user can leave a comment.

--- accounts/forms.py
+++ accounts/forms.py
@@ -21,6 +21,8 @@ class RegistrationForm(UserCreationForm):
             "first_name",
             "last_name",
             "email",
+            "password1",
+            "password2"
         )
 
     def save(self, commit=True):
